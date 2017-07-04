873ce1d4348de8f77b20c3464f52526ca0be6ec0
peterfaul <peter.faul.glass@gmail.com>
2017-07-04 10:20:34 +0200

    changed everythng

--- gittie.py
+++ gittie.py
@@ -1,5 +1,6 @@
 def introduce():
-    print("Hello, I'm Gitti\n")
+
+    print("Hello, I'm Gittie!)
 
 introduce()
 
@@ -8,6 +9,8 @@ def joke():
     print("I can't believe I made it anywhere creatively, though, because I was raised by two loving and supportive parents. Nothing squashes creativity more than unconditional love and support from a functional household. If you have kids, sh*t on their dreams a little bit.")
 
 joke()
+
+
 def add(a, b):
 
     print(int(a + b))
