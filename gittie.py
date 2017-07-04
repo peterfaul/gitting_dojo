d418436bf4081220b58e0d533e2c63dfc20d22d8
somaSzolyak <soma.szolyak@gmail.com>
2017-07-04 10:07:45 +0200

    joke

--- gittie.py
+++ gittie.py
@@ -2,3 +2,9 @@ def introduce():
     print("Hello, I'm Gitti\n")
 
 introduce()
+
+
+def joke():
+    print("I can't believe I made it anywhere creatively, though, because I was raised by two loving and supportive parents. Nothing squashes creativity more than unconditional love and support from a functional household. If you have kids, sh*t on their dreams a little bit.")
+
+joke()
